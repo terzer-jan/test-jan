5a2661ddade3406d5d92b37f0301e51b01e8ac38
unknown <terzer.jan@gmail.com>
2020-11-17 21:21:57 +0100

    Init commit

---
+++ server.js
@@ -0,0 +1,40 @@
+
+var my = require('./myModule.js');
+
+var console = require('console');
+var sys = { 'puts': console.log, 'debug': console.error },
+my_http = require("http"),
+path = require("path"),
+url = require("url"),
+filesys = require("fs");
+my_http.createServer(function(request,response){
+  console.log(request.url);
+  var my_path = url.parse(request.url).pathname;
+  console.log(my_path);
+  var full_path = path.join(process.cwd(),my_path);
+  console.log(full_path);
+  filesys.exists(full_path,function(exists){
+    if(!exists){
+      response.writeHeader(404, {"Content-Type": "text/plain"});  
+      response.write("404 Not Found\n");  
+      response.end();
+    }
+    else{
+      filesys.readFile(full_path, "binary", function(err, file) {  
+           if(err) {  
+               response.writeHeader(500, {"Content-Type": "text/plain"});  
+               response.write(err + "\n");  
+               response.end();  
+          
+           }  
+         else{
+          response.writeHeader(200);  
+              response.write(file, "binary");  
+              response.end();
+        }
+            
+      });
+    }
+  });
+}).listen(8080);
+sys.puts("Server started Running on 8080 at " + my.dateTime()); 
